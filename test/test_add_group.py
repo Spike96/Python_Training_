cbe6bb6e7c4346e78f9741f4e947a53aedc15d58
Spike96 <mtvs72@gmail.com>
2017-03-25 13:33:32 +0200

    Added checks for comparing of lists

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,8 +4,13 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.group.create(Group(name="new_group", header="groups", footer="groups_0"))
-
+    old_groups = app.group.get_group_list()
+    app.group.create(Group(name="new_group", header="groups_1", footer="groups_0"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
