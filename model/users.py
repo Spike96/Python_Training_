28438d3d42a3b2a602eefc72b01ca2c55e1fcea1
Spike96 <mtvs72@gmail.com>
2017-04-02 09:10:32 +0300

    Parametrization is created for test_add_user

--- model/users.py
+++ model/users.py
@@ -17,7 +17,8 @@ class Users:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s;%s;%s;%s;%s;%s" % (self.id, self.firstname, self.lastname, self.address, self.homephone,
+                                            self.workphone, self.mobilephone, self.email)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname, \
--- test/test_add_user.py
+++ test/test_add_user.py
@@ -1,17 +1,40 @@
 # -*- coding: utf-8 -*-
 
 from model.users import Users
+import pytest
+import random
+import string
 
 
-def test_add_user(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Users(firstname="", lastname="", email="", homephone="", workphone="",
+                  mobilephone="", address="")] + [
+    Users(firstname=random_string("fname ", 7), lastname=random_string("lname ", 10),
+          email=random_string("email ", 12), homephone=random_string("home ", 6),
+          workphone=random_string("work ", 6), mobilephone=random_string("mobile ", 9),
+          address=random_string("", 8))
+    for i in range(4)
+]
+
+
+@pytest.mark.parametrize("users", testdata, ids=[str(x) for x in testdata])
+def test_add_user(app, users):
     old_users = app.users.get_users_list()
-    users = Users(firstname="Vasya", lastname="Vasko", email="vasya@mail.com",
-                  homephone=12345, workphone=123456, mobilephone=1234567, address="somewhere")
     app.users.create(users)
     assert len(old_users) + 1 == app.users.count()
     new_users = app.users.get_users_list()
     old_users.append(users)
     assert sorted(old_users, key=Users.id_or_max) == sorted(new_users, key=Users.id_or_max)
+
+
+
+
+
+
 
 
 '''def test_add_empty_user(app):
