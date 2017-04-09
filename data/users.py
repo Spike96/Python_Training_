6dc2a972f421887b1f378de948e43fb1562934ae
Spike96 <mtvs72@gmail.com>
2017-04-09 12:46:41 +0300

    Implemented a download of data from file_json

---
+++ data/users.py
@@ -0,0 +1,8 @@
+from model.users import Users
+
+
+testdata = [Users(firstname="firstname1", lastname="lastname1", email="email1", homephone="homephone1",
+                  workphone="workphone1", mobilephone="mobilephone1", address="address1"),
+            Users(firstname="firstname2", lastname="lastname2", email="email2", homephone="homephone2",
+                  workphone="workphone2", mobilephone="mobilephone2", address="address2")
+]
--- test/test_add_user.py
+++ test/test_add_user.py
@@ -2,27 +2,28 @@
 
 from model.users import Users
 import pytest
-import random
-import string
+# import random
+# import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+# def random_string(prefix, maxlen):
+#     symbols = string.ascii_letters + string.digits + " "*10
+#     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Users(firstname="", lastname="", email="", homephone="", workphone="",
-                  mobilephone="", address="")] + [
-    Users(firstname=random_string("fname ", 7), lastname=random_string("lname ", 10),
-          email=random_string("email ", 12), homephone=random_string("home ", 6),
-          workphone=random_string("work ", 6), mobilephone=random_string("mobile ", 9),
-          address=random_string("", 8))
-    for i in range(4)
-]
+# testdata = [Users(firstname="", lastname="", email="", homephone="", workphone="",
+#                   mobilephone="", address="")] + [
+#     Users(firstname=random_string("fname ", 7), lastname=random_string("lname ", 10),
+#           email=random_string("email ", 12), homephone=random_string("home ", 6),
+#           workphone=random_string("work ", 6), mobilephone=random_string("mobile ", 9),
+#           address=random_string("", 8))
+#     for i in range(4)
+# ]
 
 
-@pytest.mark.parametrize("users", testdata, ids=[str(x) for x in testdata])
-def test_add_user(app, users):
+# @pytest.mark.parametrize("users", testdata, ids=[str(x) for x in testdata])
+def test_add_user(app, json_users):
+    users = json_users
     old_users = app.users.get_users_list()
     app.users.create(users)
     assert len(old_users) + 1 == app.users.count()
