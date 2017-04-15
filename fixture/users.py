a719032ca4bc576d2da83eed3de4610fe4e77d92
Spike96 <mtvs72@gmail.com>
2017-04-15 18:38:09 +0300

    Completed def delete_user_by_id

--- fixture/users.py
+++ fixture/users.py
@@ -61,11 +61,11 @@ class UsersHelper:
         # submit deletion
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
+        wd.find_elements_by_name("selected[]")
         self.user_cache = None
 
     def select_user_by_id(self, id):
         wd = self.app.wd
-        # wd.find_element_by_css_selector("input[id='%d']" % id).click()
         wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
     def count(self):
